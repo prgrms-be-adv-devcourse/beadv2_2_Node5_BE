f3134f213b2de5b22d3de544b82001a6309c3c10
honey720 <honey720@kyonggi.ac.kr>
2025-12-04 14:22:52 +0900

    fix: 패키지명 수정

---
+++ payment-service/src/test/java/com/node5/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.node5.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
