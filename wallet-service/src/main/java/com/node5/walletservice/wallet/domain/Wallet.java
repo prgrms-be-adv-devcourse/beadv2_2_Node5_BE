1df2bb61d36bc0f28d25e70c5f8c8a60aee6f065
honey720 <honey720@kyonggi.ac.kr>
2025-12-06 23:46:01 +0900

    feat: wallet-service의 엔티티 생성

---
+++ wallet-service/src/main/java/com/node5/walletservice/wallet/domain/Wallet.java
@@ -0,0 +1,43 @@
+package com.node5.walletservice.wallet.domain;
+
+import com.node5.common.domain.BaseEntity;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Schema(description = "예치금")
+@Table(name = "\"wallet\"", schema = "public")
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Wallet extends BaseEntity {
+
+    // 예치금 ID
+    @Id
+    private UUID id;
+
+    // 회원 ID
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID memberId;
+
+    // 잔액
+    @Column(nullable = false)
+    private Long balance;
+
+    public void deposit(Long amount) {
+        this.balance += amount;
+    }
+
+    public void withdraw(Long amount) {
+        this.balance -= amount;
+    }
+
+    @Builder
+    private Wallet(UUID memberId) {
+        this.id = UUID.randomUUID();
+        this.memberId = memberId;
+        this.balance = 0L;
+    }
+}
