deb3a4d63e37be6d1616e9c45a4d3714f5944274
honey720 <honey720@kyonggi.ac.kr>
2025-12-07 15:15:57 +0900

    fix: 예치금 환불 성공 로직에 paymentKey가 null일때의 예외사항 추가.

--- wallet-service/src/main/java/com/node5/walletservice/wallet/application/WalletService.java
+++ wallet-service/src/main/java/com/node5/walletservice/wallet/application/WalletService.java
@@ -175,6 +175,10 @@ public class WalletService {
             throw new IllegalArgumentException("환불 가능한 상태가 아닙니다.");
         }
 
+        if (walletDepositLog.getPaymentKey() == null) {
+            throw new IllegalArgumentException("결제 키가 존재하지 않습니다.");
+        }
+
         if (!walletDepositLog.getPaymentKey().equals(command.paymentKey())) {
             throw new IllegalArgumentException("결제 키가 일치하지 않습니다.");
         }
