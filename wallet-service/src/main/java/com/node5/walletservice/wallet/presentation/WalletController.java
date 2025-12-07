f800632f8d7dfa78a34f00731c6e4bd39887ecc3
honey720 <honey720@kyonggi.ac.kr>
2025-12-07 14:54:38 +0900

    refactor: ResponseEntity 래핑 방식 변경

--- wallet-service/src/main/java/com/node5/walletservice/wallet/presentation/WalletController.java
+++ wallet-service/src/main/java/com/node5/walletservice/wallet/presentation/WalletController.java
@@ -13,6 +13,7 @@ import com.node5.walletservice.wallet.presentation.dto.WalletWithdrawRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -26,61 +27,61 @@ public class WalletController {
 
     @Operation(summary = "예치금 조회", description = "회원의 예치금 정보를 조회한다.")
     @GetMapping("{memberId}")
-    public ApiResponseDto<WalletInfo> getWallet(@PathVariable UUID memberId) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> getWallet(@PathVariable UUID memberId) {
         return walletService.getWallet(memberId);
     }
 
     @Operation(summary = "예치금 생성", description = "회원의 예치금 계좌를 생성한다.")
     @PostMapping("{memberId}")
-    public ApiResponseDto<WalletInfo> createWallet(@PathVariable UUID memberId) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> createWallet(@PathVariable UUID memberId) {
         return  walletService.createWallet(memberId);
     }
 
     @Operation(summary = "예치금 입금 내역 조회", description = "회원의 예치금 입금 내역을 조회한다.")
     @GetMapping("{memberId}/deposits")
-    public PagedResponseDto<WalletDepositInfo> getDepositLogs(@PathVariable UUID memberId, Pageable pageable) {
+    public ResponseEntity<ApiResponseDto<PagedResponseDto<WalletDepositInfo>>> getDepositLogs(@PathVariable UUID memberId, Pageable pageable) {
         return walletService.getDeposits(memberId, pageable);
     }
 
     @Operation(summary = "예치금 출금 내역 조회", description = "회원의 예치금 출금 내역을 조회한다.")
     @GetMapping("{memberId}/withdraws")
-    public PagedResponseDto<WalletWithdrawInfo> getWithdraws(@PathVariable UUID memberId, Pageable pageable) {
+    public ResponseEntity<ApiResponseDto<PagedResponseDto<WalletWithdrawInfo>>> getWithdraws(@PathVariable UUID memberId, Pageable pageable) {
         return walletService.getWithdraws(memberId, pageable);
     }
 
     @Operation(summary = "예치금 충전", description = "회원의 예치금을 충전한다.")
     @PutMapping("{memberId}/charge")
-    public ApiResponseDto<WalletInfo> charge(@PathVariable UUID memberId, @RequestBody WalletChargeRequest request) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> charge(@PathVariable UUID memberId, @RequestBody WalletChargeRequest request) {
         return walletService.chargeWallet(memberId, request.toCommand());
     }
 
     @Operation(summary = "예치금 정산", description = "회원의 예치금을 정산받는다.")
     @PutMapping("{memberId}/settle")
-    public ApiResponseDto<WalletInfo> settle(@PathVariable UUID memberId, @RequestBody WalletSettleRequest request) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> settle(@PathVariable UUID memberId, @RequestBody WalletSettleRequest request) {
         return walletService.settleWallet(memberId, request.toCommand());
     }
 
     @Operation(summary = "예치금 사용", description = "회원의 예치금을 상품 주문에 사용한다.")
     @PutMapping("{memberId}/withdraw")
-    public ApiResponseDto<WalletInfo> withdraw(@PathVariable UUID memberId, @RequestBody WalletWithdrawRequest request) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> withdraw(@PathVariable UUID memberId, @RequestBody WalletWithdrawRequest request) {
         return walletService.withdrawWallet(memberId, request.toCommand());
     }
 
     @Operation(summary = "예치금 환불 요청", description = "회원의 예치금을 환불 요청한다.")
     @PutMapping("{memberId}/refund")
-    public ApiResponseDto<WalletInfo> requestRefund(@PathVariable UUID memberId, @RequestBody WalletRefundRequest request) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> requestRefund(@PathVariable UUID memberId, @RequestBody WalletRefundRequest request) {
         return walletService.requestRefundWallet(memberId, request.toCommand());
     }
 
     @Operation(summary = "예치금 환불 성공", description = "회원의 예치금 환불이 성공한다.")
     @PutMapping("{memberId}/refund/success")
-    public ApiResponseDto<WalletInfo> successRefund(@PathVariable UUID memberId, @RequestBody WalletRefundRequest request) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> successRefund(@PathVariable UUID memberId, @RequestBody WalletRefundRequest request) {
         return walletService.confirmRefundWallet(memberId, request.toCommand());
     }
 
     @Operation(summary = "예치금 환불 실패", description = "회원의 예치금 환불이 실패한다.")
     @PutMapping("{memberId}/refund/fail")
-    public ApiResponseDto<WalletInfo> failRefund(@PathVariable UUID memberId, @RequestBody WalletRefundRequest request) {
+    public ResponseEntity<ApiResponseDto<WalletInfo>> failRefund(@PathVariable UUID memberId, @RequestBody WalletRefundRequest request) {
         return walletService.failRefundWallet(memberId, request.toCommand());
     }
 
