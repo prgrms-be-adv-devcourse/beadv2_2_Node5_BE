9565f1a42d5621e226ffd27722565e8119977fc2
honey720 <honey720@kyonggi.ac.kr>
2025-12-09 22:25:08 +0900

    refactor: WalletErrorCode 작성

--- billing-service/src/main/java/com/node5/billingservice/wallet/exception/WalletErrorCode.java
+++ billing-service/src/main/java/com/node5/billingservice/wallet/exception/WalletErrorCode.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 
 @Getter
 public enum WalletErrorCode implements BaseErrorCode {
-    WALLET_NOT_FOUND(404, "WALLET_001", "예치금이 존재하지 않습니다.");
+    WALLET_NOT_FOUND(404, "WALLET_001", "예치금이 존재하지 않습니다."),
+    WALLET_LOG_NOT_FOUND(404, "WALLET_002", "예치금 내역이 존재하지 않습니다."),
+    PAYMENT_KEY_NOT_FOUND(404, "WALLET_003", "결제키가 존재하지 않습니다."),
+    PAYMENT_KEY_MISMATCH(400, "WALLET_004", "결제키가 일치하지 않습니다."),
+    REFUND_STATE_INVALID(400, "WALLET_005", "환불 상태가 유효하지 않습니다."),
+    INSUFFICIENT_WALLET_BALANCE(400, "WALLET_006", "예치금 잔액이 부족합니다.");
 
     private final int status;
     private final String code;
