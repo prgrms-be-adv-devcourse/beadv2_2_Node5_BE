2952bfa883c10122b8bd4e328ab5cc20c67d9f8d
jiinnnnnnn <saltycreamlatte@naver.com>
2025-12-10 12:23:01 +0900

    refactor: rename ProductIndexConfig to ElasticsearchIndexConfig

--- catalog-service/src/main/java/com/node5/catalogservice/search/config/ElasticsearchIndexConfig.java
+++ catalog-service/src/main/java/com/node5/catalogservice/search/config/ElasticsearchIndexConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ProductIndexConfig {
+public class ElasticsearchIndexConfig {
 
 	@Value("${app.search.product-index-name:products}")
 	private String indexName;
