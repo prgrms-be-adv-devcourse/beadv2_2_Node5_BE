25ea37967a5fdcebd8dae4a44b998bb75fa95e71
honey720 <honey720@kyonggi.ac.kr>
2025-12-11 14:35:29 +0900

    refactor: Wallet 도메인 레포지토리 변경

--- billing-service/src/main/java/com/node5/billingservice/wallet/infrastructure/WalletWithdrawLogRepositoryAdapter.java
+++ billing-service/src/main/java/com/node5/billingservice/wallet/infrastructure/WalletWithdrawLogRepositoryAdapter.java
@@ -2,11 +2,13 @@ package com.node5.billingservice.wallet.infrastructure;
 
 import com.node5.billingservice.wallet.domain.WalletWithdrawLog;
 import com.node5.billingservice.wallet.domain.WalletWithdrawLogRepository;
+import com.node5.billingservice.wallet.domain.WalletWithdrawLogState;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -17,6 +19,11 @@ public class WalletWithdrawLogRepositoryAdapter implements WalletWithdrawLogRepo
     @Override
     public Page<WalletWithdrawLog> findAllByMemberId(UUID memberId, Pageable pageable) {
         return walletWithdrawLogJpaRepository.findAllByMemberId(memberId, pageable);
+    }
+
+    @Override
+    public Optional<WalletWithdrawLog> findByOrderId(UUID orderId) {
+        return walletWithdrawLogJpaRepository.findByOrderId(orderId);
     }
 
     @Override
