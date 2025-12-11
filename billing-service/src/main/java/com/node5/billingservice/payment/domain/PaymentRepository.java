f0f4630825f0af2e6a3de19a275fe5a49f8eb9fa
honey720 <honey720@kyonggi.ac.kr>
2025-12-11 09:50:21 +0900

    feat: Payment 레포지토리 및 어댑터 구현

---
+++ billing-service/src/main/java/com/node5/billingservice/payment/domain/PaymentRepository.java
@@ -0,0 +1,16 @@
+package com.node5.billingservice.payment.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PaymentRepository {
+
+    Page<Payment> findAllByWalletId(UUID walletId, Pageable pageable);
+
+    Optional<Payment> findByOrderId(String orderId);
+
+    Payment save(Payment payment);
+}
