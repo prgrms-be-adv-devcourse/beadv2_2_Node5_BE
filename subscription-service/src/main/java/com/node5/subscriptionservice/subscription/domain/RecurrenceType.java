4c20ac98d52d74f8c47c4fee08371fc093acd371
YuYeonho <yuyeonho0619@gmail.com>
2025-12-11 18:05:03 +0900

    chore: 예외처리 중복부분 제거

--- subscription-service/src/main/java/com/node5/subscriptionservice/subscription/domain/RecurrenceType.java
+++ subscription-service/src/main/java/com/node5/subscriptionservice/subscription/domain/RecurrenceType.java
@@ -4,15 +4,9 @@ public enum RecurrenceType {
     MONTHLY, WEEKLY;
 
     public static RecurrenceType from(String value) {
-        if (value == null) {
-            throw new IllegalArgumentException("RecurrenceType not included"); //빼도될듯
-        }
-
         return java.util.Arrays.stream(values())
                 .filter(v -> v.name().equalsIgnoreCase(value))
                 .findFirst()
-                .orElseThrow(() ->
-                        new IllegalArgumentException("Invalid RecurrenceType input: " + value) //빼도될듯
-                );
+                .orElse(null);
     }
 }
