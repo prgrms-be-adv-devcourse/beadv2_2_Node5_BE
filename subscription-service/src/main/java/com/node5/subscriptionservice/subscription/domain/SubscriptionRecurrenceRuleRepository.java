929136aae337e7cfe44d36ada6fe332b832d1754
YuYeonho <yuyeonho0619@gmail.com>
2025-12-15 09:52:50 +0900

    refactor: 구독 업데이트 필드 조정

--- subscription-service/src/main/java/com/node5/subscriptionservice/subscription/domain/SubscriptionRecurrenceRuleRepository.java
+++ subscription-service/src/main/java/com/node5/subscriptionservice/subscription/domain/SubscriptionRecurrenceRuleRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface SubscriptionRecurrenceRuleRepository {
-    List<SubscriptionRecurrenceRule> findBySubscriptionId(UUID subscriptionId);
+    List<SubscriptionRecurrenceRule> findAllBySubscriptionId(UUID subscriptionId);
 
     List<SubscriptionRecurrenceRule> saveAll(List<SubscriptionRecurrenceRule> rules);
 
