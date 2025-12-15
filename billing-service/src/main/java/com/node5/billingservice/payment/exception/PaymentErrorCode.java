35af796e38ffeb30d68fb92e06f276dff3133f36
Dongheon Lee <101120544+honey720@users.noreply.github.com>
2025-12-15 14:58:29 +0900

    refactor: 개선된 상태 코드 사용으로 결제 및 지갑 오류 코드 수정 (#117)
    * refactor: 개선된 상태 코드 사용으로 결제 및 지갑 오류 코드 수정
    
    * refactor: build.gradle 수정
    
    * refactor: build.gradle 수정

--- billing-service/src/main/java/com/node5/billingservice/payment/exception/PaymentErrorCode.java
+++ billing-service/src/main/java/com/node5/billingservice/payment/exception/PaymentErrorCode.java
@@ -3,14 +3,16 @@ package com.node5.billingservice.payment.exception;
 import com.node5.common.exception.BaseErrorCode;
 import lombok.Getter;
 
+import static org.springframework.http.HttpStatus.*;
+
 @Getter
 public enum PaymentErrorCode implements BaseErrorCode {
 
-    PAYMENT_NOT_FOUND(404, "PAYMENT_001", "결제 정보를 찾을 수 없습니다."),
-    PAYMENT_KEY_MISMATCH(400, "PAYMENT_002", "결제 키가 일치하지 않습니다."),
-    PAYMENT_AMOUNT_MISMATCH(400, "PAYMENT_003", "결제 금액이 일치하지 않습니다."),
-    TOSS_SECRET_KEY_NOT_FOUND(500, "PAYMENT_004", "토스 시크릿 키가 설정되어 있지 않습니다."),
-    PAYMENT_MEMBER_ID_MISMATCH(400, "PAYMENT_005", "결제 회원 ID가 일치하지 않습니다.");
+    PAYMENT_NOT_FOUND(NOT_FOUND.value(), "PAYMENT_001", "결제 정보를 찾을 수 없습니다."),
+    PAYMENT_KEY_MISMATCH(BAD_REQUEST.value(), "PAYMENT_002", "결제 키가 일치하지 않습니다."),
+    PAYMENT_AMOUNT_MISMATCH(BAD_REQUEST.value(), "PAYMENT_003", "결제 금액이 일치하지 않습니다."),
+    TOSS_SECRET_KEY_NOT_FOUND(INTERNAL_SERVER_ERROR.value(), "PAYMENT_004", "토스 시크릿 키가 설정되어 있지 않습니다."),
+    PAYMENT_MEMBER_ID_MISMATCH(BAD_REQUEST.value(), "PAYMENT_005", "결제 회원 ID가 일치하지 않습니다.");
 
 
     private final int status;
