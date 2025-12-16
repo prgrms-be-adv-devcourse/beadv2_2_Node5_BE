041e11841e078fdea6935513fd32780e609b028e
YuYeonho <yuyeonho0619@gmail.com>
2025-12-16 23:48:02 +0900

    fix: 주문 생성 api에 맞게 코드  수정

--- subscription-service/src/main/java/com/node5/subscriptionservice/subscription/client/dto/OrderCreateRequest.java
+++ subscription-service/src/main/java/com/node5/subscriptionservice/subscription/client/dto/OrderCreateRequest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.UUID;
 
 public record OrderCreateRequest(
-        UUID memberId,
         String orderType,
         UUID subscriptionId,
         String recipientName,
