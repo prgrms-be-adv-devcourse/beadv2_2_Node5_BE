9d0018392f58b847b98a45ea880ff31f3a1bb5e1
honey720 <honey720@kyonggi.ac.kr>
2025-12-19 16:29:03 +0900

    chore: 불필요한 어노테이션 삭제

--- billing-service/src/main/java/com/node5/billingservice/wallet/presentation/WalletInternalController.java
+++ billing-service/src/main/java/com/node5/billingservice/wallet/presentation/WalletInternalController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-import static org.springframework.http.HttpStatus.CREATED;
-
 @RestController
 @RequestMapping("/internal/wallets")
 @RequiredArgsConstructor
