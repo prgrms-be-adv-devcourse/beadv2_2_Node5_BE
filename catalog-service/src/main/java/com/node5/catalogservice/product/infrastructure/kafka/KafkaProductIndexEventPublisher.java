7d523c948d26f20172fd9eab44fbda569ac11a83
jiinnnnnnn <saltycreamlatte@naver.com>
2026-01-05 19:00:03 +0900

    refactor: 트랜잭션 커밋 이후에만 상품 색인 이벤트 발행

--- catalog-service/src/main/java/com/node5/catalogservice/product/infrastructure/kafka/KafkaProductIndexEventPublisher.java
+++ catalog-service/src/main/java/com/node5/catalogservice/product/infrastructure/kafka/KafkaProductIndexEventPublisher.java
@@ -3,10 +3,12 @@ package com.node5.catalogservice.product.infrastructure.kafka;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import com.node5.catalogservice.product.event.ProductIndexEvent;
 import com.node5.catalogservice.product.application.port.ProductIndexEventPort;
 import com.node5.catalogservice.product.domain.Product;
+import com.node5.catalogservice.product.event.ProductIndexEvent;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,12 +25,25 @@ public class KafkaProductIndexEventPublisher implements ProductIndexEventPort {
 
 	@Override
 	public void publishCreate(Product product) {
-		send(ProductIndexEvent.create(product));
+		publishAfterCommit(ProductIndexEvent.create(product));
 	}
 
 	@Override
 	public void publishUpdate(Product product) {
-		send(ProductIndexEvent.update(product));
+		publishAfterCommit(ProductIndexEvent.update(product));
+	}
+
+	private void publishAfterCommit(ProductIndexEvent event) {
+		if (TransactionSynchronizationManager.isActualTransactionActive()) {
+			TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+				@Override
+				public void afterCommit() {
+					send(event);
+				}
+			});
+		} else {
+			send(event);
+		}
 	}
 
 	private void send(ProductIndexEvent event) {
