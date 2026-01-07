1dfdb901a7ae8dba6e9d0baed2bc5fc7dae72fa1
jiinnnnnnn <saltycreamlatte@naver.com>
2026-01-07 10:17:42 +0900

    feat: 검색 자동완성 API 추가

---
+++ catalog-service/src/main/java/com/node5/catalogservice/search/presentation/ProductAutocompleteController.java
@@ -0,0 +1,31 @@
+package com.node5.catalogservice.search.presentation;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.node5.catalogservice.product.domain.ProductCategory;
+import com.node5.catalogservice.search.application.ProductAutocompleteService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.v1}/products/autocomplete")
+public class ProductAutocompleteController {
+
+	private final ProductAutocompleteService productAutocompleteService;
+
+	@GetMapping
+	public ResponseEntity<List<String>> autocomplete(
+		@RequestParam String keyword,
+		@RequestParam(required = false) ProductCategory category,
+		@RequestParam(required = false) Integer size
+		) {
+		return ResponseEntity.ok(productAutocompleteService.autocomplete(keyword, category, size));
+	}
+}
