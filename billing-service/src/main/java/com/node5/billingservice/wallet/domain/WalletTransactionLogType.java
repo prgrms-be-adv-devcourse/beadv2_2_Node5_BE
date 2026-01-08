4bf13e640a46f666cd8275cb6815b67ad45bb141
honey720 <honey720@kyonggi.ac.kr>
2026-01-09 00:22:51 +0900

    feat: 예치금 조회 기록 도메인 구현

---
+++ billing-service/src/main/java/com/node5/billingservice/wallet/domain/WalletTransactionLogType.java
@@ -0,0 +1,10 @@
+package com.node5.billingservice.wallet.domain;
+
+public enum WalletTransactionLogType {
+        ORDER,
+        ORDER_REFUND,
+        SETTLEMENT,
+        TRANSFER,
+        CHARGE,
+        CHARGE_CANCEL
+}
