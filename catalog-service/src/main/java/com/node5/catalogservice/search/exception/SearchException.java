dc57497325f2f2c8a4229f44c0ece4a04b6954ac
jiinnnnnnn <saltycreamlatte@naver.com>
2026-01-08 17:22:37 +0900

    refactor: ProductSearchService 예외를 BaseException 기반으로 통일

--- catalog-service/src/main/java/com/node5/catalogservice/search/exception/SearchException.java
+++
@@ -1,9 +0,0 @@
-package com.node5.catalogservice.search.exception;
-
-import com.node5.common.exception.BaseException;
-
-public class SearchException extends BaseException {
-	public SearchException(SearchErrorCode errorCode) {
-		super(errorCode);
-	}
-}
