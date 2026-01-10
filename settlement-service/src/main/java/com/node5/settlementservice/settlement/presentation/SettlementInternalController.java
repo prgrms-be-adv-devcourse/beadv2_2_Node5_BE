b9e4b27c4790ea3a044cf034932518963a1f88a1
eujjong18 <eujjong18@naver.com>
2026-01-11 02:55:30 +0900

    refactor: 정산 배치 Job 실행 로직 서비스단으로 이동 - 컨트롤러에 존재하던 Job 실행 로직을 서비스단으로 이동 - 배치 시작 실패 시 커스텀 예외 처리

--- settlement-service/src/main/java/com/node5/settlementservice/settlement/presentation/SettlementInternalController.java
+++ settlement-service/src/main/java/com/node5/settlementservice/settlement/presentation/SettlementInternalController.java
@@ -3,24 +3,13 @@ package com.node5.settlementservice.settlement.presentation;
 import com.node5.settlementservice.settlement.application.SettlementInternalService;
 import com.node5.settlementservice.settlement.application.dto.JobExecutionInfo;
 import com.node5.settlementservice.settlement.application.dto.SettlementSourceItem;
-import com.node5.settlementservice.settlement.exception.SettlementException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-
-import static com.node5.settlementservice.settlement.exception.SettlementErrorCode.SETTLEMENT_ALREADY_COMPLETED;
 
 @Tag(name = "Settlement", description = "정산 API")
 @RestController
@@ -28,26 +17,15 @@ import static com.node5.settlementservice.settlement.exception.SettlementErrorCo
 @RequiredArgsConstructor
 public class SettlementInternalController {
 
-    private final JobLauncher jobLauncher;
-    private final Job shopSettlementJob;
     private final SettlementInternalService settlementInternalService;
 
     @Operation(summary = "전체 판매자 월별 정산 배치 실행", description = "지정 연도/월 또는 지난달(default) 기준 모든 판매자(Shop)에 대해 정산 배치 Job을 실행한다.")
     @PostMapping("/run-all")
     public ResponseEntity<String> runAll(
             @RequestParam(value = "yearMonth", required = false) String yearMonth
-    ) throws Exception {
-        JobParameters params = getDefaultSettlementParamsBuilder(yearMonth)
-                .toJobParameters();
-
-        JobExecution jobExecution;
-        try {
-            jobExecution = jobLauncher.run(shopSettlementJob, params);
-        } catch(JobInstanceAlreadyCompleteException e) {
-            throw new SettlementException(SETTLEMENT_ALREADY_COMPLETED);
-        }
-
-        return ResponseEntity.ok(String.format("Settlement job started for all shops - BatchId: %s", jobExecution.getId()));
+    ) {
+        Long jobExecutionId = settlementInternalService.runAll(yearMonth);
+        return ResponseEntity.ok(String.format("Settlement job started for all shops - BatchId: %s", jobExecutionId));
     }
 
     @Operation(summary = "특정 판매자 월별 정산 배치 실행", description = "지정 연도/월 또는 지난달(default) 기준 특정 판매자(Shop)에 대해 정산 배치 Job을 실행한다.")
@@ -55,19 +33,9 @@ public class SettlementInternalController {
     public ResponseEntity<String> runShop(
             @RequestParam("shopId") String shopId,
             @RequestParam(value = "yearMonth", required = false) String yearMonth
-    ) throws Exception {
-        JobParameters params = getDefaultSettlementParamsBuilder(yearMonth)
-                .addString("shopId", shopId)
-                .toJobParameters();
-
-        JobExecution jobExecution;
-        try {
-            jobExecution = jobLauncher.run(shopSettlementJob, params);
-        } catch(JobInstanceAlreadyCompleteException e) {
-            throw new SettlementException(SETTLEMENT_ALREADY_COMPLETED);
-        }
-
-        return ResponseEntity.ok(String.format("Settlement job started for shop(%s) - BatchId: %s", shopId, jobExecution.getId()));
+    ) {
+        Long jobExecutionId = settlementInternalService.runShop(shopId, yearMonth);
+        return ResponseEntity.ok(String.format("Settlement job started for shop(%s) - BatchId: %s", shopId, jobExecutionId));
     }
 
     @Operation(summary = "단일 배치 실행 상태 조회", description = "배치 ID(jobExecutionId)를 통해 해당 Job의 상세 상태를 조회한다.")
@@ -83,25 +51,6 @@ public class SettlementInternalController {
     ) {
         settlementInternalService.saveSettlementResource(items);
         return ResponseEntity.ok().build();
-    }
-
-    private JobParametersBuilder getDefaultSettlementParamsBuilder(String yearMonth){
-        YearMonth targetMonth;
-        if(yearMonth != null && !yearMonth.isEmpty()){
-            targetMonth = YearMonth.parse(yearMonth, DateTimeFormatter.ofPattern("yyyy-MM"));
-        }else{
-            // 지정하지 않을 경우 지난 달을 기본값으로 설정
-            targetMonth = YearMonth.now().minusMonths(1);
-        }
-
-        // YearMonth 객체로 시작일, 종료일 계산
-        String startDate = targetMonth.atDay(1).format(DateTimeFormatter.ISO_DATE);
-        String endDate = targetMonth.atEndOfMonth().format(DateTimeFormatter.ISO_DATE);
-
-        return new JobParametersBuilder()
-                //.addLong("timestamp", System.currentTimeMillis())
-                .addString("startDate", startDate)
-                .addString("endDate", endDate);
     }
 
 }
