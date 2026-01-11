9fee43b7cf55777b73d2c74fa231b47cd6731c32
jiinnnnnnn <saltycreamlatte@naver.com>
2026-01-11 17:42:53 +0900

    refactor: 장바구니 수정/삭제 시 카트 자동 생성 방지

--- catalog-service/src/main/java/com/node5/catalogservice/cart/exception/CartErrorCode.java
+++ catalog-service/src/main/java/com/node5/catalogservice/cart/exception/CartErrorCode.java
@@ -12,6 +12,7 @@ public enum CartErrorCode implements BaseErrorCode {
 	CART_PRODUCT_NOT_FOUND(404, "CART_PRODUCT_NOT_FOUND", "장바구니에 담으려는 상품이 존재하지 않습니다."),
 	CART_PRODUCT_NOT_ON_SALE(400, "CART_PRODUCT_NOT_ON_SALE", "해당 상품은 장바구니에 담을 수 없는 상태입니다."),
 	CART_ITEM_NOT_FOUND(404, "CART_ITEM_NOT_FOUND", "장바구니 항목이 존재하지 않습니다."),
+	CART_NOT_FOUND(404, "CART_NOT_FOUND", "장바구니가 존재하지 않습니다."),
 	CART_QUANTITY_INVALID(400, "CART_QUANTITY_INVALID", "수량은 1 이상이어야 합니다.");
 
 	private final int status;
