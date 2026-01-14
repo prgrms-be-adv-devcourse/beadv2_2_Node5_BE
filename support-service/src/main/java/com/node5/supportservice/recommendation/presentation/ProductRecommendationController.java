1649f8ebefe708df15b35adb838e2543d54470a7
YuYeonho <127312366+yuuuyeonho@users.noreply.github.com>
2026-01-14 11:14:12 +0900

    fix (#355)

--- support-service/src/main/java/com/node5/supportservice/recommendation/presentation/ProductRecommendationController.java
+++ support-service/src/main/java/com/node5/supportservice/recommendation/presentation/ProductRecommendationController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("${api.v1}/recommendations")
+@RequestMapping("${api.v1}/recommendation")
 @RequiredArgsConstructor
 public class ProductRecommendationController {
 
