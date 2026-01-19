c3b73b83dcd4117ba8fa7c5bdd70b28be16c851c
honey720 <honey720@kyonggi.ac.kr>
2026-01-19 17:37:24 +0900

    refactor: shop-service를 member-service로 수정

--- catalog-service/src/main/java/com/node5/catalogservice/shop/client/ShopOwnershipClient.java
+++ catalog-service/src/main/java/com/node5/catalogservice/shop/client/ShopOwnershipClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "shop-service")
+@FeignClient(name = "member-service")
 public interface ShopOwnershipClient {
 
 	@GetMapping("/internal/shops/{shopId}/member-id")
