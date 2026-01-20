15fcdefc8dd4f7a0f7a4174d1ffe98246568292f
Dongheon Lee <101120544+honey720@users.noreply.github.com>
2026-01-20 16:34:15 +0900

    refactor: 설정 수정 (#462)

--- batch-service/src/main/java/com/node5/batchservice/config/OpenFeignConfig.java
+++ batch-service/src/main/java/com/node5/batchservice/config/OpenFeignConfig.java
@@ -1,11 +1,13 @@
 package com.node5.batchservice.config;
 
-import com.node5.batchservice.payment.client.PaymentClient;
+import com.node5.batchservice.reviewsummary.client.CatalogClient;
 import com.node5.batchservice.subscription.client.OrderClient;
+import com.node5.batchservice.payment.client.PaymentClient;
+import com.node5.batchservice.reviewsummary.client.SupportClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableFeignClients(clients = {OrderClient.class, PaymentClient.class})
+@EnableFeignClients(clients = {CatalogClient.class, OrderClient.class, PaymentClient.class, SupportClient.class})
 public class OpenFeignConfig {
 }
