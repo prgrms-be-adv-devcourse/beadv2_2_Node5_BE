ec9b7aa4952b924ccad105d0c825835603f823ac
jiinnnnnnn <saltycreamlatte@naver.com>
2026-01-20 19:32:40 +0900

    chore: 사용하지 않는 ProductSearchRepository 제거

--- support-service/src/main/java/com/node5/supportservice/search/infrastructure/ProductSearchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.node5.supportservice.search.infrastructure;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.node5.supportservice.search.domain.ProductDocument;
-
-public interface ProductSearchRepository extends ElasticsearchRepository<ProductDocument, String> {
-}
