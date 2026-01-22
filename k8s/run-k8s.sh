24b5a331b25efd85dbbd21abc5ee35ce1838a629
eujjong18 <eujjong18@naver.com>
2026-01-23 01:24:15 +0900

    refactor: k8s 파일 수정 (settlement 제거)

--- k8s/run-k8s.sh
+++ k8s/run-k8s.sh
@@ -15,7 +15,7 @@ $KUBECTL apply -k k8s
 $KUBECTL get all
 
 for deploy in apigateway catalog-service discovery \
-  member-service order-service settlement-service \
+  member-service order-service \
   payment-service wallet-service; do
   $KUBECTL rollout status "deploy/${deploy}" || true
 done
