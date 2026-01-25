244e8f835faea28d48210c93d32998ee633c6e7c
eujjong18 <eujjong18@naver.com>
2026-01-25 16:32:47 +0900

    fix: 정산 요청 대상 데이터 필터링 조건 추가 - 정산 요청 데이터 필터링 조건에 settlementStatus 추가 - 구매확정된 상품이 없을 경우 요청을 생략하도록 수정 - OrderStatus에서 환불 및 정산 요청 관련 상태 제거

--- order-service/src/main/java/com/node5/orderservice/order/domain/OrderStatus.java
+++ order-service/src/main/java/com/node5/orderservice/order/domain/OrderStatus.java
@@ -10,8 +10,6 @@ public enum OrderStatus {
     PAID("결제완료"),
     PAYMENT_FAILED("결제실패"),
     CANCELED("주문취소"),
-    REFUNDED("환불완료"),
-    SETTLEMENT_REQUESTED("정산준비")
     ;
 
     private final String name;
